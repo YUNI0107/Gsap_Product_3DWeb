3155ce7741bb27353b6e7920cd21ff4a3024d80d
YUNI0107 <amy123chou@gmail.com>
2023-11-02 20:28:51 +0800

    Add: add gsap animation to cube

--- src/experience/world/Word.ts
+++ src/experience/world/Word.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import Experience from '../Experience.js'
+import gsap from 'gsap'
 
 class World {
   experience: Experience
@@ -14,6 +15,8 @@ class World {
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     const cube = new THREE.Mesh(geometry, material)
     this.scene.add(cube)
+
+    gsap.to(cube.rotation, { duration: 2, y: 3, repeat: -1, yoyo: true })
   }
 
   update() {}
